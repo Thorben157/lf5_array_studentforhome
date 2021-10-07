db5a632c8cdbeeda2bfcb341ee544c332cf22511
thorben157 <thorben157@schule.bremen.de>
2021-10-07 15:22:28 +0200

    ArrayCalculator done

--- src/arraySum2d/ArraySum2d.java
+++ src/arraySum2d/ArraySum2d.java
@@ -2,10 +2,23 @@ package arraySum2d;
 
 public class ArraySum2d {
     public int sumAll(int[][] array) throws IllegalArgumentException {
-        return 0;
+
+        int eins = 0;
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                eins += array[i][j];
+            }
+        }
+        return eins;
     }
 
     public int[] rowSums(int[][] array) throws IllegalArgumentException {
+
+
+
+
+
         return null;
     }
 
